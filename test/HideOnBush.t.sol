2c74fb914e0660c0078791dd6679a47830f84199
Harry Chen <a00012025@gmail.com>
2023-12-04 00:05:53 +0800

    feat: new cha

---
+++ test/HideOnBush.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/hide-on-bush/Challenge.sol";
+
+contract ContractTest is Test {
+  
+}
